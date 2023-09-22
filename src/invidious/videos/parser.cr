986784f66404cd83c75e339633c5cc236c628ed0
syeopite <syeopite@syeopite.dev>
2023-09-22 10:34:26 -0400

    Add option for HLS quality in user preferences

--- src/invidious/videos/parser.cr
+++ src/invidious/videos/parser.cr
@@ -50,7 +50,7 @@ def parse_related_video(related : JSON::Any) : Hash(String, JSON::Any)?
   }
 end
 
-def extract_video_info(video_id : String)
+def extract_video_info(video_id : String, force_hls : Bool = false)
   # Init client config for the API
   client_config = YoutubeAPI::ClientConfig.new
 
@@ -101,20 +101,24 @@ def extract_video_info(video_id : String)
   params["reason"] = JSON::Any.new(reason) if reason
 
   new_player_response = nil
-
-  if reason.nil?
-    # Fetch the video streams using an Android client in order to get the
-    # decrypted URLs and maybe fix throttling issues (#2194). See the
-    # following issue for an explanation about decrypted URLs:
-    # https://github.com/TeamNewPipe/NewPipeExtractor/issues/562
-    client_config.client_type = YoutubeAPI::ClientType::AndroidTestSuite
+  if force_hls
+    client_config.client_type = YoutubeAPI::ClientType::IOS
     new_player_response = try_fetch_streaming_data(video_id, client_config)
-  end
+  else
+    if reason.nil?
+      # Fetch the video streams using an Android client in order to get the
+      # decrypted URLs and maybe fix throttling issues (#2194). See the
+      # following issue for an explanation about decrypted URLs:
+      # https://github.com/TeamNewPipe/NewPipeExtractor/issues/562
+      client_config.client_type = YoutubeAPI::ClientType::AndroidTestSuite
+      new_player_response = try_fetch_streaming_data(video_id, client_config)
+    end
 
-  # Last hope
-  if new_player_response.nil?
-    client_config.client_type = YoutubeAPI::ClientType::TvHtml5ScreenEmbed
-    new_player_response = try_fetch_streaming_data(video_id, client_config)
+    # Last hope
+    if new_player_response.nil?
+      client_config.client_type = YoutubeAPI::ClientType::TvHtml5ScreenEmbed
+      new_player_response = try_fetch_streaming_data(video_id, client_config)
+    end
   end
 
   # Replace player response and reset reason
